15200451671d73162cff86b716571fc6f6009020
Sam Patankar <sampatankar@gmail.com>
2023-09-07 18:13:01 +0100

    tech with nana first changes

---
+++ one.py
@@ -0,0 +1,13 @@
+"""
+starts with overview:
+- basic building blocks of programming
+- modules
+- project: countdown app
+- packages, PyPI and pip
+- Project: automation with Python
+- OOP - classes and objects
+- Project - API request to GitLab
+
+
+
+"""
