739833c8e220b146c823c01c978cbd6655c06ca6
Sam Patankar <sampatankar@gmail.com>
2023-09-07 18:45:20 +0100

    final changes on day 1

--- one.py
+++ one.py
@@ -9,3 +9,19 @@ starts with overview:
 - Project - API request to GitLab
 
 """
+
+#Strings & Numbers:
+print(200, "is a great number")
+
+print(20 * 24 * 60)
+
+
+#String concatenation:
+print(f"20 days are {20 * 24 * 60} minutes")
+
+
+#Encapsulate Login in Functions:
+"""
+start at 39:33 tomorrow
+"""
+
