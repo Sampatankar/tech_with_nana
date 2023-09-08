830d85d12a4b4ab905c409d1a1a9be2ef9b21abe
Sam Patankar <sampatankar@gmail.com>
2023-09-08 18:50:33 +0100

    day 2 - up to dinner time

--- one.py
+++ one.py
@@ -22,6 +22,60 @@ print(f"20 days are {20 * 24 * 60} minutes")
 
 #Encapsulate Login in Functions:
 """
-start at 39:33 tomorrow
+Allows us to not repeat logic, and encapsulate an action in one place
+"""
+calculation_to_units = 24
+name_of_unit = "hours"
+
+def days_to_units(num_of_days, custom_message):
+    print(f"{num_of_days} days are {num_of_days * calculation_to_units} {name_of_unit}")
+    print(custom_message)
+
+"""
+using return instead is also a possibility if we don't need to print a string
+"""
+
+days_to_units(35, "you are a *ick!")
+
+
+#Scope:
+"""
+Discussion around global and local variables and in what data structures each are accessible.
+"""
+
+
+#Accepting User Input:
+"""
+gateways for users to access your codes actions
+"""
+your_name = input("What is your name? ")
+print(your_name)
+
+
+#Conditionals & If/Else (with comparison operators):
+your_age = input("What is your age? (enter a number please) ")
+arbitrary_old_age_no = 44
+
+if int(your_age) > arbitrary_old_age_no:
+    print("You're old! Bought a tombstone yet?")
+else:
+    print("You'll age like fine wine! Go enjoy life!")
+
+"""
+we could handle problems with invalid input in several ways. stripping out anything that isn't what we want? (regex).
+Or we could use an .isdigit() builtin function and nesting the if/else written above inside the .isdigit function nested in a root if/else
+"""
+
+
+#Error Handling with Try/Except:
+"""
+You can next Try/Excepts within a function for example. The Try part will encapsulate the action you want to happen.
+The Except part can be used to capture a system or logic response on the production of any one of Python's syntax, or runtime(exceptions).
+"""
+
+
+#While Loops:
+"""
+Lets take a break for dinner! Continue from 1:50:12, while loops
 """
 
